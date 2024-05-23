60ac7cf5266a0499aeca01e4a10fb6e97b2e0fcc
Zied <zinzied@gmail.com>
2024-05-23 13:54:58 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from time import time
+import time
 import random
 import os
 import sys
